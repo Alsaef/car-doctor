68e6fab01b0570dfc6d20ee75823c46c47b47f0d
Alsaef <saef.ratul@gmail.com>
2025-08-09 22:19:28 +0600

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import myImage from '../assets/images/about_us/person.jpg'
+
+const About = () => {
+  return (
+    <div className='my-12'>
+      <div className="flex items-center justify-center px-12 gap-3">
+        {/* Left side image */}
+        <div className="">
+          <img src={myImage} alt="Sample" className="rounded-2xl w-[490px] h-[473px]" />
+        </div>
+
+        {/* Right side text */}
+        <div className="p-6">
+          <h2 className="text-xl text-orange-500 font-extrabold mb-4">About Us</h2>
+
+          <p className="mt-2 text-4xl  font-bold text-gray-800 ">
+            We are qualified <br />
+            & of experience <br />
+            in this field
+          </p>
+
+          <p className="mt-2 text-sm text-gray-600 leading-relaxed text-justify">
+            There are many variations of passages of Lorem Ipsum <br /> available,
+            but the majority have suffered alteration in some <br /> form, by injected humour,
+            or randomised words which don't <br /> look even slightly believable.
+          </p>
+
+          <p className="mt-2 text-sm text-gray-600 leading-relaxed text-justify">
+            The majority have suffered alteration in some form, by injected <br /> humour,
+            or randomised words which don't look even slightly <br /> believable.
+          </p>
+
+          <button className="btn bg-[#FF3811] text-white mt-7">
+            Get More Info
+          </button>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
